e82454d65fa3896eaf6fadbb73299dcb21a845b9
Slemp123 <36300877+Slemp123@users.noreply.github.com>
2019-04-09 16:15:57 +0200

    Småfix

--- header.php
+++ header.php
@@ -7,29 +7,30 @@
 	
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Log In</title>
 	<link rel="stylesheet" type="text/css" href="">
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-
 	 <header>
-	 	<div class="container">
-	 	<h1>Forum Placeholder</h1>
-	 	
-	 	 <nav>
-	 		 <ul>
-	 		 	<li><a href="feed.php">FEED</a></li>
-	 		 	<li><a href="#">KONTAKT</a></li>
-	 		 	<li><a href="minSide.php">MIN SIDE</a></li>
-	 		 	<li><a href="#">FAQ</a></li>
-	 		 </ul>
+    <div class="container">
+        <div id="branding">
+          <h1>Forum Placeholder</h1>
+        </div></div>
+     
+      <nav class="container">
+          <ul> 
+              <li><a href=feed.php>     Feed       </a></li> 
+              <li><a href=#>            Kontakt    </a></li> 
+              <li><a href=minSide.php>  Min Side     </a></li> 
+              <li><a href=#>            FAQ        </a></li>
+          </ul>
+      </nav>
 	 		 <?php
 	 		 	if (isset($_SESSION['userId'])) {
-	  			echo '<form class="formHeader" action="includes/logout.inc.php" method="post">
-	 		 		<button type="submit" name="logout-submit">Logout</button>
+	  			echo '<form class="header" action="includes/logout.inc.php" method="post">
+	 		 		<button type="submit" class="logout-submit" name="logout-submit">Logout</button>
 	 		 	</form>';
-	  		}
+	  }
 	  		else {
 	  			  header("Location: http://localhost/applikasjon/index.php", true, 301);
 exit();
