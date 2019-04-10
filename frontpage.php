cd565144ba430609beb814f55db48e0b564ed3e5
Slemp123 <36300877+Slemp123@users.noreply.github.com>
2019-04-10 21:25:12 +0200

    Frontpage

---
+++ frontpage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" type="text/css" href="">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+	 <header>
+	 	<div class="container">
+    		<img src="bilder/yippee.png" alt="">
+    	</div>
+	</header>
+		<body>
+
+    <section id="intro" class="intro">
+		<div class="slogan">
+			<h1>Møte appen for studenter</h1>
+			<p>Møt andre studenter fra ditt campus, <br> bli med på og lag arrangementer. <br>
+				Register deg i dag!</p>
+		<a href="signup.php" class="login-submit">Register deg</a>
+		<br> <br> <br>
+			<p> Allerede en bruker? </p>
+		<a href="index.php" class="login-submit">Logg inn</a>
+		</div>	
+   
+
+</section>
+
+
+</body>
+</html>
